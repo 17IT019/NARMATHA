a53bfd6952cf3cff7e739f8c296330080ca2d616
17IT019 <54097869+17IT019@users.noreply.github.com>
2019-08-16 15:52:39 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter a num");
+    scanf("%d",&d);
+    if(num%2==0)
+     printf("%d even",num);
+    else
+     printf("%d odd",num);
+    }
+
+    return 0;
+}
